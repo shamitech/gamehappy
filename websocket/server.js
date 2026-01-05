246d03f1da7b43fddeee5016c935bb4692801ebf
jaredshami <jaredshami1979@gmail.com>
2026-01-05 16:57:19 -0700

    Add CORS headers to allow test dashboard to access /test-game endpoint

--- websocket/server.js
+++ websocket/server.js
@@ -18,6 +18,18 @@ const io = new Server(server, {
   }
 });
 
+// Add CORS headers for HTTP requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://gamehappy.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Initialize game server
 const gameServer = new GameServer();
 
